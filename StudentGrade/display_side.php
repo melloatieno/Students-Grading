1089825ef9a2698a49d04498ae239c1a94646642
melloatieno <atienomelvine98@gmail.com>
2023-04-16 18:09:58 +0300

    Update display_side.php

--- StudentGrade/display_side.php
+++ StudentGrade/display_side.php
@@ -3,7 +3,7 @@
   </header>
   <aside>
     <ul>
-      <li><a href="http://localhost/StudentGrade/display.php">View Student Grades</a></li>
-      <li><a href="http://localhost/StudentGrade/addrecord.php">Add Student Grade</a></li>
+      <li><a href="display.php">View Student Grades</a></li>
+      <li><a href="addrecord.php">Add Student Grade</a></li>
     </ul>
   </aside>
